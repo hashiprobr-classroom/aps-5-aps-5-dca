2239d25911753dc2f5ab4ea8a872ed64c4711612
mariaebs <166644824+mariaebs@users.noreply.github.com>
2025-03-20 22:47:23 -0300

    Update DataTempo.java

--- insee/src/main/java/br/edu/insper/desagil/aps5/insee/DataTempo.java
+++ insee/src/main/java/br/edu/insper/desagil/aps5/insee/DataTempo.java
@@ -1,11 +1,11 @@
 package br.edu.insper.desagil.aps5.insee;
 
-public class DataTempo extends Referencia{
+public class DataTempo extends Referencia {
     private Data data;
     private Tempo tempo;
 
-    public DataTempo(String observacao, Data data, Tempo tempo) {
-        super(observacao);
+    public DataTempo(Data data, Tempo tempo) {
+        super();
         this.data = data;
         this.tempo = tempo;
     }
@@ -18,12 +18,13 @@ public class DataTempo extends Referencia{
         return tempo;
     }
 
-                /*
     @Override
     public int comoInteiro() {
 
+        return data.comoInteiro() * 1440 + tempo.comoInteiro();
 
     }
+}
 
          */
 }
